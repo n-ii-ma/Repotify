124e877c0c40c389e1cf25ad8b8981f659447f10
Assassin Nima <88039431+n-ii-ma@users.noreply.github.com>
2021-12-06 18:22:01 +0330

    CSS Update

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,12 @@
 import Search from "../components/Search";
 import TrackList from "../components/TrackList";
+import "../reset.css";
 import "../App.css";
 
 function App() {
   return (
     <div className="App">
-      <div className='container'>
+      <div className="container">
         <header>
           <h1>Top Tracks from the Artists You Love</h1>
         </header>
